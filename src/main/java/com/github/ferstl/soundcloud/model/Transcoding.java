4d94144f58adcec1e572626aee2f66ffc4ca9faf
Stefan Ferstl <st.ferstl@gmail.com>
2019-06-16 23:19:46 +0200

    Use String for URLs

--- src/main/java/com/github/ferstl/soundcloud/model/Transcoding.java
+++ src/main/java/com/github/ferstl/soundcloud/model/Transcoding.java
@@ -1,18 +1,17 @@
 package com.github.ferstl.soundcloud.model;
 
-import java.net.URL;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public class Transcoding {
 
-  private final URL url;
+  private final String url;
   private final long duration;
   private final String quality;
   private final String preset;
   private final Format format;
 
   @JsonCreator
-  public Transcoding(URL url, long duration, String quality, String preset, Format format) {
+  public Transcoding(String url, long duration, String quality, String preset, Format format) {
     this.url = url;
     this.duration = duration;
     this.quality = quality;
@@ -20,7 +19,7 @@ public class Transcoding {
     this.format = format;
   }
 
-  public URL getUrl() {
+  public String getUrl() {
     return this.url;
   }
 
